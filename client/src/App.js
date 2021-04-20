41177e25604f4f9f218fe3e9192521a64c02914f
Ludmila <notforgiving@list.ru>
2021-04-20 12:58:24 +0300

    starts over

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,7 @@
-import { useEffect } from "react";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-
-import { authentification } from "./redux/actions/autorizateAcrion";
-
-import Main from "./pages/Main/Main";
-import LoginPage from "./pages/Login/Login";
-import Favorites from "./pages/Favorites/Favorites";
-
-import style from "./generalStyle.module.css";
-import "./theme.css";
-import "antd/dist/antd.css";
-
 function App() {
-  const dispatch = useDispatch();
-  const { auth } = useSelector((state) => state);
-
-  useEffect(() => {
-    dispatch(authentification());
-  }, [dispatch]);
-
   return (
-    <div className={style.container}>
-      <BrowserRouter>
-        {!auth.isAuth ? (
-          <Switch>
-            <Route exact path="/login" component={LoginPage} />
-            <Redirect to='/login'/>
-          </Switch>
-        ) : (
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/favorites" component={Favorites} />
-            <Redirect to="/"/>
-          </Switch>
-        )}
-      </BrowserRouter>
+    <div>
+
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,7 @@
 import ReactDOM from "react-dom";
-
-import { Provider } from "react-redux";
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
-
-import store from "./redux/store";
 import App from "./App"
 
-const history = createBrowserHistory();
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+      <App />,
   document.getElementById("root")
 );
